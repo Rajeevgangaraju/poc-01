c86a9b967c7fbc5fb072f9f9396070d6950df400
Rajeevgangaraju <134952680+Rajeevgangaraju@users.noreply.github.com>
2026-04-22 22:36:26 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,1 +1,12 @@
+package com.example;
 
+public class App {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("DevSecOps POC is running");
+    }
+}
